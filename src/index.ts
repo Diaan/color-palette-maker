ee8a1ce2bb87f54f1af6a06439ccbd4be96480d4
Diana Broeders <diana.broeders@sanoma.com>
2024-04-08 19:48:32 +0200

    fix path

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@ export * from './events';
 export * from './color-palette-maker';
 export * from './color-palette/color-card';
 export * from './components';
-
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-setBasePath('/assets/icons');
